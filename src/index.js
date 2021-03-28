9daafe721ec399a74b2a268cdc785f8365b6ea80
kequin <panasiktrofim@gmail.com>
2021-03-28 22:30:29 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
